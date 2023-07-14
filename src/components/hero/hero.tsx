e89dd05100c1d5a3c77c56bf587d223052a41f73
jokerav <jokeravdeveloper@gmail.com>
2023-07-14 15:36:08 +0300

    hero fix

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -4,17 +4,20 @@ import {ReactComponent as Arrow} from "img/svg/arrow.svg";
 const Hero = () => {
     return (
         <section className={s.hero}>
-            <div className={s.titleWrapper}>
-                <h1 className={s.heroTitle}>Новые поступления в этом сезоне</h1>
-                <p className={s.heroSubTitle}>Утонченные сочетания и бархатные оттенки - вот то, что вы искали в этом
-                    сезоне. Время исследовать.</p>
-                <div className={s.btnWrapper}>
-
+            <div className={s.heroContainer}>
+                <div className={s.backgroundContainer}>
+                    <div className={s.orderWrapper}>
+                    <h1 className={s.heroTitle}>Новые поступления в этом сезоне</h1>
+                    <p className={s.heroSubTitle}>Утонченные сочетания и бархатные оттенки - вот то, что вы искали в
+                        этом
+                        сезоне. Время исследовать.</p>
+                    <div className={s.btnWrapper}>
                         <div className={s.arrowBtn}><Arrow/></div>
                         <button className={s.openShopBtn}>Открыть магазин</button>
-                    </div>
+                    </div></div>
                 </div>
+            </div>
         </section>
-)
+    )
 }
 export default Hero;
